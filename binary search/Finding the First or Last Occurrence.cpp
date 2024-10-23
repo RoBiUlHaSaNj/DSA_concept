4c662ed787ec09740e8814f67648ebd87d25f52d
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-10-23 18:53:05 +0600

    Finding the First or Last Occurrence.cpp

---
+++ binary search/Finding the First or Last Occurrence.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int findFirstOccurrence(int arr[], int n, int target) {
+    int left = 0, right = n - 1, result = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            result = mid; // Update result and search left for first occurrence
+            right = mid - 1;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+}
+
+
+int findLastOccurrence(int arr[], int n, int target) {
+    int left = 0, right = n - 1, result = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            result = mid; // Update result and search right for last occurrence
+            left = mid + 1;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+}
